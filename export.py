a258385f1df5641e92c42717561ba8b6d311ec72
Naveen Unnikrishnan <naveenunnikrishnan98@gmail.com>
2024-09-15 13:10:14 -0400

    Improvements in the export script
    - Change default export to slugs.
    - Add option to export URLs (instead of slugs).
    - Pass headers correctly.
    - Modify env vars for session and token to be specific to export.

--- export.py
+++ export.py
@@ -6,19 +6,17 @@ import requests
 from operator import attrgetter
 
 
-def parse_args() -> tuple[str, str, str, str]:
+def parse_args() -> tuple[str, str, str, str, str]:
     parser = argparse.ArgumentParser(
-        description="Get the URLs of questions in a leetcode list."
+        description="Get the questions in a leetcode list."
     )
-    parser.add_argument(
-        "list_slug", type=str, help="The list to fetch"
-    )
+    parser.add_argument("list_slug", type=str, help="The list to fetch")
     parser.add_argument(
         "--session",
         "-s",
         type=str,
         dest="leetcode_session",
-        default=os.environ.get("LEETCODE_SESSION"),
+        default=os.environ.get("LC_EXPORT_SESSION"),
         help="Leetcode session (get this from your browser)",
     )
     parser.add_argument(
@@ -26,8 +24,16 @@ def parse_args() -> tuple[str, str, str, str]:
         "-t",
         type=str,
         dest="csrf_token",
-        default=os.environ.get("LEETCODE_CSRF_TOKEN"),
+        default=os.environ.get("LC_EXPORT_TOKEN"),
         help="Leetcode CSRF token (get this from your browser)",
+    )
+    parser.add_argument(
+        "--urls",
+        "-u",
+        dest="print_urls",
+        default=False,
+        action="store_true",
+        help="Print URLs instead of question slugs",
     )
     parser.add_argument(
         "--sl-no",
@@ -35,22 +41,31 @@ def parse_args() -> tuple[str, str, str, str]:
         dest="add_sl_no",
         default=False,
         action="store_true",
-        help="Add serial numbers to each URL",
+        help="Add serial numbers to each question slug/URL",
     )
     args = parser.parse_args()
-    return attrgetter("list_slug", "leetcode_session", "csrf_token", "add_sl_no")(args)
+    return attrgetter(
+        "list_slug", "leetcode_session", "csrf_token", "print_urls", "add_sl_no"
+    )(args)
 
 
-def get_cookies(csrf_token: str, session: str) -> dict[str, str]:
+def get_headers(csrf_token: str) -> dict[str, str]:
     return {
         "x-csrftoken": csrf_token,
-        "csrftoken": csrf_token,
-        "LEETCODE_SESSION": session,
         "Referer": "https://leetcode.com",
     }
 
 
-def get_list(list_slug: str, cookies: dict[str, str]) -> list[str]:
+def get_cookies(csrf_token: str, session: str) -> dict[str, str]:
+    return {
+        "csrftoken": csrf_token,
+        "LEETCODE_SESSION": session,
+    }
+
+
+def get_list(
+    list_slug: str, cookies: dict[str, str], headers: dict[str, str]
+) -> list[str]:
     query = """
         query favoriteQuestionList($favoriteSlug: String!, $filter: FavoriteQuestionFilterInput) {
             favoriteQuestionList(favoriteSlug: $favoriteSlug, filter: $filter) {
@@ -62,17 +77,18 @@ def get_list(list_slug: str, cookies: dict[str, str]) -> list[str]:
     """
     variables = {
         "favoriteSlug": list_slug,
-        "filter": {"positionRoleTagSlug": "", "skip": 0, "limit": 100},
+        "filter": {"positionRoleTagSlug": "", "skip": 0, "limit": 1000},
     }
     try:
         response = requests.post(
             "https://leetcode.com/graphql",
             json={"query": query, "variables": variables},
             cookies=cookies,
+            headers=headers,
         )
         response.raise_for_status()
         questions = [
-            f"https://leetcode.com/problems/{question['titleSlug']}"
+            question["titleSlug"]
             for question in response.json()["data"]["favoriteQuestionList"]["questions"]
         ]
         return questions
@@ -80,18 +96,25 @@ def get_list(list_slug: str, cookies: dict[str, str]) -> list[str]:
         raise SystemExit(e)
 
 
-def print_urls(urls: list[str], add_sl_no: bool):
-    for i, url in enumerate(urls):
+def to_urls(questions: list[str]) -> list[str]:
+    return [f"https://leetcode.com/problems/{question}" for question in questions]
+
+
+def print_qs(questions: list[str], add_sl_no: bool):
+    for i, question in enumerate(questions):
         if add_sl_no:
             print(f"{i + 1}.", end="\t")
-        print(url)
+        print(question)
 
 
 def main():
-    list_slug, session, csrf_token, add_sl_no = parse_args()
+    list_slug, session, csrf_token, print_urls, add_sl_no = parse_args()
     cookies = get_cookies(csrf_token=csrf_token, session=session)
-    question_urls = get_list(list_slug=list_slug, cookies=cookies)
-    print_urls(question_urls, add_sl_no)
+    headers = get_headers(csrf_token=csrf_token)
+    questions = get_list(list_slug=list_slug, cookies=cookies, headers=headers)
+    if print_urls:
+        questions = to_urls(questions)
+    print_qs(questions, add_sl_no)
 
 
 if __name__ == "__main__":
